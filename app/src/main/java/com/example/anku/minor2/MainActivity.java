1427927cb766705630d407c571a45df1b7cd7f4a
megha43 <meghamittal4396@gmail.com>
2017-03-01 12:55:14 +0530

    Updated version

--- app/src/main/java/com/example/anku/minor2/MainActivity.java
+++ app/src/main/java/com/example/anku/minor2/MainActivity.java
@@ -15,10 +15,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.facebook.FacebookSdk;
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.PhoneNumber;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 import com.larswerkman.holocolorpicker.ColorPicker;
 import com.larswerkman.holocolorpicker.OpacityBar;
 import com.larswerkman.holocolorpicker.SVBar;
@@ -30,14 +40,62 @@ public class MainActivity extends ActionBarActivity {
 
     private FButton LoginBtn;
     private FButton SignupBtn;
+    public static int APP_REQUEST_CODE = 99;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AccountKit.initialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         LoginBtn = (FButton) findViewById(R.id.login_button);
         SignupBtn = (FButton) findViewById(R.id.signup_button);
+
+
+
+        AccessToken accessToken = AccountKit.getCurrentAccessToken();
+        if (accessToken != null) {
+            Intent i=new Intent(this,After.class);
+            finish();
+            startActivity(i);
+        } else {
+
+        }
+    //    LoginBtn=(FButton) findViewById(R.id.button);
+        LoginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                final Intent intent = new Intent(getApplicationContext(), AccountKitActivity.class);
+                AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                        new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                                AccountKitActivity.ResponseType.TOKEN);
+
+                //EditText phone_number;
+
+                //name= (EditText) findViewById(R.id.editText);
+                //phone_number=(EditText) findViewById(R.id.phonenumber);
+               // PhoneNumber p=new PhoneNumber("91",phone_number.getText()+"","");
+               // configurationBuilder.setInitialPhoneNumber(p);
+                configurationBuilder.setReadPhoneStateEnabled(true);
+                configurationBuilder.setReceiveSMS(true);
+                intent.putExtra(
+                        AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                        configurationBuilder.build());
+                startActivityForResult(intent, APP_REQUEST_CODE);
+            }
+        });
+
+
+
+
+
+
     }
+
+
 
 
     @Override
@@ -47,5 +105,52 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+
+    @Override
+    protected void onActivityResult(
+            final int requestCode,
+            final int resultCode,
+            final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_REQUEST_CODE) { // confirm that this response matches your request
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if (loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+                Toast.makeText(this,
+                        "error",
+                        Toast.LENGTH_LONG)
+                        .show();
+                //showErrorActivity(loginResult.getError());
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+            } else {
+                if (loginResult.getAccessToken() != null) {
+                    toastMessage = "Login Successful";
+
+                } else {
+                    toastMessage = String.format(
+                            "Login Successful"                        );
+                }
+
+                Intent nextScreen = new Intent(this , After.class);
+                startActivity(nextScreen);
+                Toast.makeText(this,
+                        "Login successful",
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+
+            // Surface the result to your user in an appropriate way.
+       /*     Toast.makeText(
+                    this,
+                    toastMessage,
+                    Toast.LENGTH_LONG)
+                    .show();
+       */ }
+    }
+
 
 }
